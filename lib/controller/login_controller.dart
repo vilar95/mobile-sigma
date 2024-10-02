a1150b05a9de9d6a912bd4a08d4629a9e4638781
Vilar95 <e.vilar95@gmail.com>
2024-10-02 20:58:18 -0300

    Adiciona login_controller com mobx;

---
+++ lib/controller/login_controller.dart
@@ -0,0 +1,35 @@
+import 'package:mobx/mobx.dart';
+
+part 'login_controller.g.dart';
+
+class LoginController = _LoginControllerBase with _$LoginController;
+
+abstract class _LoginControllerBase with Store {
+  @observable
+  String email = '';
+
+  @observable
+  String password = '';
+
+  @observable
+  bool isLoading = false;
+
+  @observable
+  String errorMessage = '';
+
+  @action
+  void setEmail(String value) {
+    email = value;
+  }
+
+  @action
+  void setPassword(String value) {
+    password = value;
+  }
+
+  @action
+  Future<void> login() async {
+    isLoading = true;
+    errorMessage = '';
+  }
+}
