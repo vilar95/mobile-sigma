5e723a7e3a86fd7d0d49ac55068e7f4f78a535af
Vilar95 <e.vilar95@gmail.com>
2024-10-29 15:44:24 -0300

    Remove comentários;

--- test/support_screen_test.dart
+++ test/support_screen_test.dart
@@ -3,12 +3,10 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:sigma/screens/support_screen.dart';
 
 void main() {
-  // Configuração inicial
+
   setUp(() {
-    // Qualquer configuração necessária antes de cada teste
   });
 
-  // Teste: Verifica se a tela de suporte é renderizada corretamente
   testWidgets('Verifica se a tela de suporte é renderizada corretamente',
       (WidgetTester tester) async {
     await tester.pumpWidget(const MaterialApp(home: SupportScreen()));
@@ -18,7 +16,6 @@ void main() {
     expect(find.byType(ElevatedButton), findsOneWidget);
   });
 
-  // Teste: Verifica se os campos de texto estão presentes
   testWidgets('Verifica se os campos de texto estão presentes',
       (WidgetTester tester) async {
     await tester.pumpWidget(const MaterialApp(home: SupportScreen()));
@@ -28,7 +25,6 @@ void main() {
     expect(find.text('No campo abaixo, digite suas dúvidas ou sugestões de melhorias:'), findsOneWidget);
   });
 
-  // Teste: Verifica se o botão de envio está presente
   testWidgets('Verifica se o botão de envio está presente',
       (WidgetTester tester) async {
     await tester.pumpWidget(const MaterialApp(home: SupportScreen()));
@@ -36,16 +32,12 @@ void main() {
     expect(find.text('Enviar'), findsOneWidget);
   });
 
-  // Teste: Verifica se a validação do formulário funciona corretamente
   testWidgets('Verifica se a validação do formulário funciona corretamente',
       (WidgetTester tester) async {
     await tester.pumpWidget(const MaterialApp(home: SupportScreen()));
 
     await tester.tap(find.text('Enviar'));
     await tester.pump();
-
-    // expect(find.text('Por favor, preencha este campo'),
-    //     findsNWidgets(2)); // Supondo que há duas mensagens de erro
   });
 
 }
