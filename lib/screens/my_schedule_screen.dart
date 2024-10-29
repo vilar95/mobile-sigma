24ea04657c084a1c20487fc5255d0f34faf3983f
Vilar95 <e.vilar95@gmail.com>
2024-10-28 21:03:09 -0300

    Remove imports;

--- lib/screens/my_schedule_screen.dart
+++ lib/screens/my_schedule_screen.dart
@@ -1,4 +1,3 @@
-import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:sigma/_core/routes/sigma_routes.dart';
 import 'package:sigma/_core/theme/sigma_colors.dart';
